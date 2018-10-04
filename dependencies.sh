467caafab595f28ff2b089a1edd8a19a7d71f085
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2018-10-04 10:11:51 +0200

    Fixed Qt dependencies.

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Needed for Qt5 on travis build server.
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+
 # Refresh package list.
-sudo apt update
+sudo apt update -qq
 
 # Qt dependencies.
 sudo apt install qtbase5-dev qttols5-dev qtmultimedia5-dev libqt5sql5-psql libqt5serialport5-dev
