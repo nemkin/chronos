e26ccd0eb6b1da9176071bc7d9204d4c5d252e68
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2018-10-04 10:05:05 +0200

    Dependency install took forever, cut it down.

--- dependencies.sh
+++ dependencies.sh
@@ -7,4 +7,4 @@ sudo apt update
 sudo apt install qtbase5-dev qttols5-dev qtmultimedia5-dev libqt5sql5-psql libqt5serialport5-dev
 
 # Latex dependencies.
-sudo apt install texlive-full
+sudo apt install --no-install-recommends texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra dvipng
