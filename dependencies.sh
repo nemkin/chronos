3a77e84438a22736da7ed2e106564ca0f652cbb2
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2018-10-04 12:08:31 +0200

    Travis fix.

--- dependencies.sh
+++ dependencies.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Needed for Qt5 on travis build server.
-sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo add-apt-repository --yes ppa:beineri/opt-qt-5.10.1-trusty
 
 # Refresh package list.
 sudo apt update -qq
 
 # Qt dependencies.
-sudo apt install qtbase5-dev qttols5-dev qtmultimedia5-dev libqt5sql5-psql libqt5serialport5-dev
+sudo apt install qt510base qt510tools qt510multimedia libqt5sql5-psql libqt5serialport5-dev
 
 # Latex dependencies.
 sudo apt install --no-install-recommends texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra dvipng
