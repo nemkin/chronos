e6fa148b41722699854c0d14c8aaf6e2076346bf
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2018-11-10 13:24:15 +0100

    Dependencies reduced.

--- dependencies.sh
+++ dependencies.sh
@@ -3,18 +3,16 @@
 sudo apt install build-essential cmake
 
 # Needed for Qt5 on travis build server.
-sudo add-apt-repository --yes ppa:beineri/opt-qt-5.11.1-bionic
-
+# sudo add-apt-repository --yes ppa:beineri/opt-qt-5.11.1-bionic
 # Refresh package list.
-sudo apt update -qq
+# sudo apt update -qq
+# Qt dependencies.
+# sudo apt install qt511base qt511tools qt511multimedia libqt5sql5-psql libqt5serialport5-dev
 
 # PostgreSQL for C++
 sudo apt install libpqxx-dev
 
-# Qt dependencies.
-# sudo apt install qt511base qt511tools qt511multimedia libqt5sql5-psql libqt5serialport5-dev
-
 # Latex dependencies.
-sudo apt install texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra dvipng
+# sudo apt install texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra dvipng
 
 # TODO: or-tools: https://github.com/google/or-tools.git
